3452d960d1aee25d95b073f70c50e508e1e9ca3f
Josh Schmelzle <josh@joshschmelzle.com>
2021-12-16 23:42:18 -0500

    test for i2c module presence

--- hwtest/tests/test_i2c.py
+++ hwtest/tests/test_i2c.py
@@ -1,5 +1,31 @@
 from hwtest.helpers import run_command
+import subprocess
 
+def i2c_present():
+    '''
+    Use modinfo to check if i2c module is loaded
+    '''
+    try:
+        cmd = "lsmod | grep i2c"
+        subprocess.check_output(cmd, shell=True)
+        return True
+    except subprocess.CalledProcessError as exc:
+        return False
+
+def test_i2c_present():
+    """
+    Test command:
+        lsmod | grep i2c
+    
+    Results:
+        True - i2c module(s) detected in lsmod
+        False - not detected
+    """
+
+    assert i2c_present() == True
+
+# enable i2c: sudo raspi-config nonint do_i2c 0
+# disable i2c: sudo raspi-config nonint do_i2c 1
 
 def test_i2c_enabled():
     """
@@ -13,4 +39,4 @@ def test_i2c_enabled():
 
     resp = run_command(["raspi-config", "nonint", "get_i2c"])
 
-    assert resp == 0
+    assert resp == '0'
