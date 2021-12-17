6585b86ae7eef9b4cec350c06d00619802ea9a6b
Josh Schmelzle <josh@joshschmelzle.com>
2021-12-16 23:45:59 -0500

    add test for bluetooth module presence

--- hwtest/tests/test_bluetooth.py
+++ hwtest/tests/test_bluetooth.py
@@ -1,5 +1,29 @@
 import subprocess
 
+def bluetooth_module_present():
+    '''
+    Use modinfo to check if bluetooth module is loaded
+    '''
+    try:
+        cmd = "lsmod | grep bluetooth"
+        subprocess.check_output(cmd, shell=True)
+        return True
+    except subprocess.CalledProcessError as exc:
+        return False
+
+
+def test_bluetooth_module_present():
+    """
+    Test command:
+        lsmod | grep bluetooth
+    
+    Results:
+        True - bluetooth module detected in lsmod
+        False - not detected
+    """
+
+    assert bluetooth_module_present() == True
+
 def bluetooth_present():
     '''
     We want to use hciconfig here as it works OK when no devices are present
