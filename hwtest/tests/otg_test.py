ca296906dbdff6aa74088b5abf9da316e70aee61
Josh Schmelzle <josh@joshschmelzle.com>
2021-12-22 14:54:03 -0600

    add otg test

---
+++ hwtest/tests/otg_test.py
@@ -0,0 +1,31 @@
+from hwtest.tests.helpers import is_module_present, run_command
+
+# Further reading: https://michael.stapelberg.ch/posts/2021-04-27-linux-usb-virtual-serial-cdc-acm/
+
+def test_RNDIS_gadget():
+    """
+    Test for idProduct 0xa4a2 Linux-USB Ethernet/RNDIS Gadget in `lsusb` output
+    """
+
+    resp = run_command(["lsusb"])
+
+    assert ":a4a2 " in resp
+    assert "RNDIS" in resp
+
+
+def test_cdc_ether_mod():
+    """
+    Test command:
+        lsmod | grep cdc_ether
+
+    Results:
+        True - cdc_ether module detected in lsmod
+        False - not detected
+
+    Description:
+        g_ether is used on the device/peripheral side, cdc_ether is used on the host side.
+
+        If we see cdc_ether loaded then we know communication is established between the 2 USB ports.
+    """
+
+    assert is_module_present("cdc_ether") == True
