c1fb0eaf7f9d8171fb470739f394be0130c8522b
crvallance <crvallance@protonmail.com>
2021-12-22 16:35:11 -0800

    included iwtest but not running

--- hwtest/tests/ax2xx_test.py
+++ hwtest/tests/ax2xx_test.py
@@ -1,7 +1,7 @@
 from hwtest.tests.helpers import run_command
 
 
-def ax2xx_devices_present_test():
+def ax2xx_devices_present_lspci():
     """
     Test command:
         lspci | grep Intel
@@ -13,3 +13,18 @@ def ax2xx_devices_present_test():
     resp = run_command(["lspci", "|", "grep", "Intel"])
 
     assert resp.count('Intel Corporation Wi-Fi 6') == 2
+
+
+def ax2xx_devices_present_iw():
+    """
+    Test command:
+        iw dev
+
+    Results:
+        True - (>=2) "#phys" are seen and (2) instances of "1c:99:57" are seen
+        False - <2 "#phys" are seen or (2) instances of "1c:99:57" are not seen
+    """
+    resp = run_command(["iw", "dev"])
+
+    assert resp.count('phy#') >= 2
+    assert resp.count('1c:99:57') == 2
