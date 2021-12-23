035e7da197b14692b25b5304507a55f6da04019f
crvallance <crvallance@protonmail.com>
2021-12-22 16:50:13 -0800

    removing iw test, mac isn't reliable

--- hwtest/tests/ax2xx_test.py
+++ hwtest/tests/ax2xx_test.py
@@ -13,18 +13,3 @@ def ax2xx_devices_present_lspci_test():
     resp = run_command(["lspci", "|", "grep", "Intel"])
 
     assert resp.count('Intel Corporation Wi-Fi 6') == 2
-
-
-def ax2xx_devices_present_iw():
-    """
-    Test command:
-        iw dev
-
-    Results:
-        True - (>=2) "#phys" are seen and (2) instances of "1c:99:57" are seen
-        False - <2 "#phys" are seen or (2) instances of "1c:99:57" are not seen
-    """
-    resp = run_command(["iw", "dev"])
-
-    assert resp.count('phy#') >= 2
-    assert resp.count('1c:99:57') == 2
