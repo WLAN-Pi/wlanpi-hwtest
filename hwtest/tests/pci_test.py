b5bc8121298dff2eb523e73bd19df82a34c2c71f
Colin Vallance <colin@cvallance.net>
2021-12-24 11:48:14 -0800

    Pci wip (#17)

--- hwtest/tests/pci_test.py
+++ hwtest/tests/pci_test.py
@@ -3,24 +3,20 @@ import pytest
 from hwtest.tests.helpers import run_command
 
 # fmt: off
-EXPECTED_PCI = [
-    ('00:00.0', 'Broadcom Inc. and subsidiaries BCM2711 PCIe Bridge'),
-    ('01:00.0', 'Pericom Semiconductor PI7C9X2G404 EL/SL PCIe2 4-Port/4-Lane Packet Switch'),
-    ('03:00.0', 'USB controller: VIA Technologies, Inc. VL805 USB 3.0 Host Controller'),
-    ('04:00.0', 'Network controller: Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz'),
-    ('05:00.0', 'Network controller: Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz')
-]
+params = {
+    'Slot 0': ('00:00.0', 'Broadcom Inc. and subsidiaries BCM2711 PCIe Bridge'),
+    'Slot 1': ('01:00.0', 'Pericom Semiconductor PI7C9X2G404 EL/SL PCIe2 4-Port/4-Lane Packet Switch'),
+    'Slot 3': ('03:00.0', 'USB controller: VIA Technologies, Inc. VL805 USB 3.0 Host Controller'),
+    'Slot 4': ('04:00.0', 'Network controller: Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz'),
+    'Slot 5': ('05:00.0', 'Network controller: Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz')
+}
 # fmt: on
 
 
-@pytest.fixture(params=EXPECTED_PCI)
-def get_lspci(request):
-    slot = request.param[0]
-    expected = request.param[1]
-    return (run_command([f"lspci -s {slot}"], invoke_shell=True), expected)
-
-
-def test_pci(get_lspci):
+@pytest.mark.parametrize(
+    "slot,expected", list(params.values()), ids=list(params.keys())
+)
+def test_pci(slot, expected):
     """
     Test command:
         lspci -s xx:xx.x
@@ -29,8 +25,7 @@ def test_pci(get_lspci):
         True - PCI slot output matches expected value from tuple
         False - PCI slot output does not expected value from tuple
     """
-
-    cmd_output, expected = get_lspci
+    cmd_output = run_command([f"lspci -s {slot}"], invoke_shell=True)
     print(f"cmd output: {cmd_output}")
     print(f"expected output: {expected}")
 
