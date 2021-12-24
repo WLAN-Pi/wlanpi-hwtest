6f1c8c360a1b244aac6b4d715c7145844f39b79b
crvallance <crvallance@gmail.com>
2021-12-24 12:45:47 -0600

    tox formatting

--- hwtest/tests/pci_test.py
+++ hwtest/tests/pci_test.py
@@ -2,6 +2,7 @@ import pytest
 
 from hwtest.tests.helpers import run_command
 
+# fmt: off
 params = {
     'Slot 0': ('00:00.0', 'Broadcom Inc. and subsidiaries BCM2711 PCIe Bridge'),
     'Slot 1': ('01:00.0', 'Pericom Semiconductor PI7C9X2G404 EL/SL PCIe2 4-Port/4-Lane Packet Switch'),
@@ -9,9 +10,12 @@ params = {
     'Slot 4': ('04:00.0', 'Network controller: Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz'),
     'Slot 5': ('05:00.0', 'Network controller: Intel Corporation Wi-Fi 6 AX210/AX211/AX411 160MHz')
 }
+# fmt: on
 
 
-@pytest.mark.parametrize('slot,expected', list(params.values()), ids=list(params.keys()))
+@pytest.mark.parametrize(
+    "slot,expected", list(params.values()), ids=list(params.keys())
+)
 def test_pci(slot, expected):
     """
     Test command:
