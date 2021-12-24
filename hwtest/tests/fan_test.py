561127295a894309b22b95d82e3cb473c09ba3e2
Josh Schmelzle <josh@joshschmelzle.com>
2021-12-24 11:48:49 -0500

    add fan test

--- hwtest/tests/fan_test.py
+++ hwtest/tests/fan_test.py
@@ -1,7 +1,7 @@
-from hwtest.tests.helpers import is_module_present
+from hwtest.tests.helpers import is_module_present, run_command
 
 
-def test_gpio_module():
+def test_gpio_fan_mod():
     """
     Test command:
         lsmod | grep gpio_fan
@@ -14,7 +14,7 @@ def test_gpio_module():
     assert is_module_present("gpio_fan") is True
 
 
-def test_gpio_config():
+def test_gpio_fan_conf():
     """
     Test command:
         read /boot/config.txt
@@ -27,3 +27,30 @@ def test_gpio_config():
     with open("/boot/config.txt", "r") as f:
         config_txt = f.read()
         assert "gpio-fan" in config_txt
+
+
+def test_fan_detected():
+    """
+    Test command: sensors
+
+    Output:
+        gpio_fan-isa-0000
+        Adapter: ISA adapter
+        fan1:        5000 RPM  (min =    0 RPM, max = 5000 RPM)
+
+    Expects 1:
+        True - gpio_fan-isa-0000 in response
+        False - did not find gpio_fan-isa-0000 in response
+
+    Expects 2)
+        True - fan0 or fan1 in the output
+        False - fan0 or fan1 NOT in output
+
+    Depends:
+        sudo apt install lm-sensors
+    """
+
+    resp = run_command(["sensors"])
+
+    assert "gpio_fan-isa-0000" in resp
+    assert "fan0" or "fan1" in resp
