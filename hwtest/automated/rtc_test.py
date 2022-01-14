cb8bb15075bf9e5240932da958c2b8271aadcd16
Josh Schmelzle <josh@joshschmelzle.com>
2022-01-14 00:39:20 -0500

    remove hwclock -r test and add /dev/rtc0 exists test (#43)

--- hwtest/automated/rtc_test.py
+++ hwtest/automated/rtc_test.py
@@ -14,18 +14,37 @@ def test_rtc_pcf85063_mod():
     assert is_module_present("rtc_pcf85063") == True
 
 
-def test_rtc_hwclock():
+def test_rtc0_exists():
     """
-    Test command:
-        hwclock -r
+        Test command:
+        ls /dev/rtc0
 
     Expect:
-        exit value is 0 (we got a timestamp)
+        exit value is 0 (/dev/rtc0 exists)
 
     Not Expected:
-        exit value is 1 (cannot access hardware clock for example)
+        exit value is 2 (/dev/rtc0 does not exist)
     """
 
-    exit_value = run_command(["hwclock", "-r"], return_exit_values=True)
+    exit_value = run_command(["ls", "/dev/rtc0"], return_exit_values=True)
 
     assert exit_value == 0
+
+################
+# INVALID TEST #
+################
+# def test_rtc_hwclock():
+#     """
+#     Test command:
+#         hwclock -r
+#
+#     Expect:
+#         exit value is 0 (we got a timestamp)
+#
+#     Not Expected:
+#         exit value is 1 (cannot access hardware clock for example)
+#     """
+#
+#     exit_value = run_command(["hwclock", "-r"], return_exit_values=True)
+#
+#     assert exit_value == 0
